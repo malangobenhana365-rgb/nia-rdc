39813214cf1ce8c27ad46203db3db78a5bbec549
malangobenhana365-rgb <malangobenhana365@gmail.com>
2026-08-10 18:27:32 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-fgghhhhh
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+  res.send('API Backend nia-rdc opérationnelle');
+});
+
+app.listen(PORT, () => {
+  console.log(`Serveur démarré sur le port ${PORT}`);
+});
